d9b93cb3a1e3e697e2c3865751927b2c6a06358f
ThibaultBougerol <67759716+svost07@users.noreply.github.com>
2022-05-04 19:48:40 +0200

    Update main.cpp

--- main/main.cpp
+++ main/main.cpp
@@ -1,20 +1,46 @@
 
 #include <iostream>
-#include "Composant1.h"
-#include "Composant2.h"
+#include <dlfcn.h>
 
 int main(int argc, char ** argv)
 {
 	int data1=3;
 	int data2=5;
 
-	int valeur1;
-	int valeur2;
-
-	valeur1=composant1(data1,data2);
-
-	valeur2=composant2(data1,data2);
-
-	std::cout << getComposant1Version() << std::endl;
-	std::cout << "valeur 1 :" << valeur1 << " valeur 2 :" << valeur2 << std::endl;
+	int valeur;
+	int choice = -1;
+	
+	//Chose the component 
+	while (choice != 1 || choice != 2){
+		stdd::cout << "Which component do you want? Type 1 or 2." << std::endl;
+		std::cin>> choice;
+	}
+	
+	//Open the right file
+	void hndl = nullptr; 
+	if (choice == 1){
+		hndl = dlopen("./libComposant1.so", RTLD_LAZY);
+	else if (choice == 2){
+		hndl = dlopen("./libComposant2.so", RTLD_LAZY);
+	}
+		
+	int (*fct) (int, int);
+	if (choice == 1) {
+		fct = (int (*) (int, int)) dlsym(hndl, "composant1");
+	} else if (choice == 2){
+		fct = (int (*) (int, int)) dlsym(hndl, "composant2");
+	}
+	
+	//Return error
+	if (!fct) {
+		std::cerr << "dlsym : " << dlerror() << std::endl;
+		exit(EXIT_FAILURE);
+	}
+	
+	//Final print 
+	std::cout << "The value is:" << fct(data1, data2) << std::endl;
+	
+	//Close 
+	dlclose(hndl);
+	exit(EXIT_SUCCESS);
 }
